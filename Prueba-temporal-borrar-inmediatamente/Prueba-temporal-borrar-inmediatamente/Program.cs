2d60ed3ed66d202e67abf61e3455d16362a5a3b8
R4 <160157540+RTCL4@users.noreply.github.com>
2024-10-10 23:03:07 -0600

    Add files via upload

---
+++ Prueba-temporal-borrar-inmediatamente/Prueba-temporal-borrar-inmediatamente/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba_temporal_borrar_inmediatamente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // esto es para ver que pasa
+        }
+    }
+}
